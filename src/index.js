bc54d49ba825fe82b2bbc34b361acf1b1d050bed
Michał Grzyśka <grzyska.michal@gmail.com>
2025-09-15 22:00:22 +0200

    Added node

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 chrome.devtools.panels.elements.createSidebarPane(
   "Copy HTML with CSS",
   function (sidebar) {
-    sidebar.setPage("src/sidebar/sidebar.html");
+    sidebar.setPage("sidebar/sidebar.html");
   }
 );
