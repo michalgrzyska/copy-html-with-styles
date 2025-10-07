df376342191b3619d0ba0bb6559f721a7fe2e02a
Michał Grzyśka <grzyska.michal@gmail.com>
2025-10-07 20:28:39 +0200

    Vite config for debugging without minifier

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,42 +2,42 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig({
-  build: {
-    outDir: "dist",
-    minify: "terser",
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "src/index.js"),
-        sidebar: resolve(__dirname, "src/sidebar/sidebar.js"),
-      },
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "chunks/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash][extname]",
-      },
+export default defineConfig(({ mode }) => ({
+    build: {
+        outDir: "dist",
+        minify: mode === "production" ? "terser" : false,
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "src/index.js"),
+                sidebar: resolve(__dirname, "src/sidebar/sidebar.js"),
+            },
+            output: {
+                entryFileNames: "[name].js",
+                chunkFileNames: "chunks/[name]-[hash].js",
+                assetFileNames: "assets/[name]-[hash][extname]",
+            },
+        },
+        terserOptions: {
+            keep_classnames: true,
+            keep_fnames: true,
+        },
     },
-    terserOptions: {
-      keep_classnames: true,
-      keep_fnames: true,
-    },
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/**/*.html",
-          dest: ".",
-        },
-        {
-          src: "src/**/*.css",
-          dest: ".",
-        },
-        {
-          src: "src/manifest.json",
-          dest: ".",
-        },
-      ],
-    }),
-  ],
-});
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "src/**/*.html",
+                    dest: ".",
+                },
+                {
+                    src: "src/**/*.css",
+                    dest: ".",
+                },
+                {
+                    src: "src/manifest.json",
+                    dest: ".",
+                },
+            ],
+        }),
+    ],
+}));
